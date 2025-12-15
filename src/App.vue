257d35bf5b558cd29c0fffc1075ad3077ed8a4f1
Densanity <denniszixiang@gmail.com>
2025-12-15 17:33:04 +0100

    made a footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="w-screen h-screen flex flex-col justify-center items-center">
+  <div class="w-screen min-h-screen flex flex-col justify-center items-center">
     <Home />
+    <Footer />
   </div>
 </template>
 
 <script setup>
 import Home from "./components/Home.vue";
+import Footer from "./components/Footer.vue";
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex felx-row gap-12 w-3/4 min-h-screen pt-24">
-    <div class="w-1/4 h-[800px] bg-gray-500 rounded-xl p-3">
+  <div class="flex felx-row gap-12 w-3/4 h-3/4 min-h-screen py-12">
+    <div class="w-1/4 h-[700px] bg-gray-500 rounded-xl p-3">
       <div class="h-1/3 w-full">Top</div>
       <div class="h-2/3 w-full">Bottom</div>
     </div>
