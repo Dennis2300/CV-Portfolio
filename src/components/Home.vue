0a95859837cf9ef66f040749d0feb3f3a564f625
Densanity <denniszixiang@gmail.com>
2025-12-15 17:50:38 +0100

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="flex felx-row gap-12 w-3/4 h-3/4 min-h-screen py-12">
-    <div class="w-1/4 h-[700px] bg-gray-500 rounded-xl p-3">
-      <div class="h-1/3 w-full">Top</div>
-      <div class="h-2/3 w-full">Bottom</div>
+  <div class="flex felx-row gap-12 w-3/4 min-h-screen py-12">
+    <div class="w-1/4 h-[800px] bg-gray-500 rounded-xl px-3">
+      <div class="w-full h-1/3 flex flex-col items-center justify-around pt-10">
+        <img src="https://placehold.co/150x150?text=Profile Picture" class="rounded-full" alt="" />
+        <h1 class="text-2xl mt-2">Dennis-Zixiang Zhou</h1>
+      </div>
+      <div class="divider px-2"></div>
+      <div class="w-full h-2/3">Bottom</div>
     </div>
     <div class="w-3/4 bg-gray-600 rounded-xl p-3">
       <h1>Content</h1>
