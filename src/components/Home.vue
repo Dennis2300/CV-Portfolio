40ee4957bee9c186ce3d59d2b639e97a54c9d775
Densanity <denniszixiang@gmail.com>
2025-12-15 19:37:58 +0100

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,12 +2,12 @@
   <div
     class="flex flex-col md:flex-row gap-12 w-full md:w-3/4 min-h-screen py-16 mx-auto"
   >
-    <Aside />
-    <Main />
+    <Aside class="md:w-1/4 w-full sticky top-2 max-h-screen overflow-auto" />
+    <Main class="md:w-3/4 w-full" />
   </div>
 </template>
 
 <script setup>
 import Aside from "./Aside.vue";
-import Main from "./Main.vue"
+import Main from "./Main.vue";
 </script>
