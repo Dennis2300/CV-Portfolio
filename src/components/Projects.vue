dfcc4cd189b0cbe8babbbef167428e7da3e2cef6
Densanity <denniszixiang@gmail.com>
2025-12-17 18:13:12 +0100

    Update Projects.vue

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1>My Projects</h1>
+  <div class="px-8 text-(--quaternary-color)">
+    <h1 class="text-5xl">My Projects</h1>
+    <div
+      class="divider w-[50px] before:bg-(--secondary-color) after:bg-(--secondary-color)"
+    ></div>
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur sint
+        fuga ratione perferendis? Inventore corporis eligendi nulla unde
+        explicabo praesentium consectetur? Quo molestiae dignissimos iure, nihil
+        reiciendis error. Quasi, possimus?
+      </p>
+    </div>
+    <div
+      class="divider before:bg-(--secondary-color) after:bg-(--secondary-color)"
+    ></div>
+    <div class="flex flex-col gap-4 w-full">
+      <div class="bg-(--bg-color) h-[200px]">
+        <img src="https://placehold.co/856x100" alt="">
+        <h1>Project here!</h1>
+      </div>
+      <div class="bg-(--bg-color) h-[200px]">
+        <h1>Project here!</h1>
+      </div>
+    </div>
+  </div>
 </template>
